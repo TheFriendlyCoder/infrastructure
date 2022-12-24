2d0bc1c3eefdd6080d21ca6653bfe3346f5f2618
Kevin Phillips <kevinp@ca.ibm.com>
2022-12-24 14:23:05 -0400

    Refactored roles to work with limited user

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -u kevin -b --become-pass-file become_pass.txt --vault-password-file=vault_pass.txt all.yml $@
+ansible-playbook -u kevin --become-pass-file become_pass.txt --vault-password-file=vault_pass.txt all.yml $@
